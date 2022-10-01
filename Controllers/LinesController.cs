d6410f832bf23f99327bb817ce7e82b9aa9d4296
huruglica <93087066+huruglica@users.noreply.github.com>
2022-10-01 23:50:41 +0200

    Connect to database

---
+++ Controllers/LinesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LifeTravel.Data;
+using LifeTravel.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LifeTravel.Controllers
+{
+    public class LinesController : ControllerBase
+    {
+        private readonly DataBase ReadFromDataBase;
+
+        public LinesController(DataBase dbContext)
+        {
+            ReadFromDataBase = dbContext;
+        }
+
+        [HttpGet("lines")]
+        public IEnumerable<Lines> Get()
+        {
+            Console.WriteLine("ketuu");
+            var AllLines = ReadFromDataBase.Lines.ToList();
+            return AllLines;
+        }
+
+        [HttpGet("line")]
+        public Lines Get(int id)
+        {
+            Console.WriteLine("tjetraa");
+            Lines filteredData;
+            filteredData = ReadFromDataBase.Lines.
+                Where(x => x.Id == id).FirstOrDefault();
+            return filteredData;
+        }
+    }
+}
---
+++ Data/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using LifeTravel.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;   
+
+namespace LifeTravel.Data
+{
+    public class DataBase : DbContext
+    {
+        public DataBase(DbContextOptions<DataBase> options) : base(options)
+        {
+
+        }
+        public DbSet<Lines> Lines { get; set; }
+    }
+}
