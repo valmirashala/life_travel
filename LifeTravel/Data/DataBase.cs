e68f366d2e9fc7e4c2dae981fd8efc5a9c3041f4
huruglica <93087066+huruglica@users.noreply.github.com>
2022-10-03 16:54:20 +0200

    add solution file
    Co-Authored-By: Valmira Shala <93986564+valmirashala@users.noreply.github.com>
    Co-Authored-By: Ardittz <106332906+Ardittz@users.noreply.github.com>
    Co-Authored-By: kaslara <106890426+kaslara@users.noreply.github.com>

---
+++ LifeTravel/Data/DataBase.cs
@@ -0,0 +1,16 @@
+﻿using LifeTravel.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;   
+
+namespace LifeTravel.Data
+{
+    public class DataBase : DbContext
+    {
+        //Constructor
+        public DataBase(DbContextOptions<DataBase> options) : base(options)
+        {
+
+        }
+        public DbSet<Lines> Lines { get; set; }
+    }
+}
