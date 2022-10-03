a86b332f2132f164db1522c244ed6cb8a22eb817
Valmira Shala <valmirashalaa@gmail.com>
2022-10-03 22:41:56 +0200

    Add Model Passanger

---
+++ LifeTravel/Model/Passanger.cs
@@ -0,0 +1,17 @@
+﻿namespace LifeTravel.Model
+{
+    public class Passanger
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public bool IsAsmin { get; set; }
+
+        public static explicit operator Passanger(List<Passanger> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
