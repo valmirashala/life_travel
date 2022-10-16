d5fbaf13614959a3cb2f03c5482eb181fa13d064
Ardittz <arditmullabazi@gmail.com>
2022-10-17 00:39:07 +0200

    updated program cs

--- Travelephant/Travelephant/Program.cs
+++ Travelephant/Travelephant/Program.cs
@@ -37,12 +37,10 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 //app.UseHttpsRedirection();
 app.UseCors();
